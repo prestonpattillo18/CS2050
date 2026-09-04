6d75f59814a886fe7b370d1c05de8b63494e97ef
prestonpattillo18 <preston.pattillo18@gmail.com>
2026-09-04 18:13:23 -0500

    Add header file with function declarations

---
+++ labs/lab1/lab1.h
@@ -0,0 +1,6 @@
+#ifndef _LAB1_H
+#define _LAB1_H
+int getAverage(int array[], int size, int *result);
+int sumNegative(int array[], int size, int *result);
+int sumEven(int array[], int size, int *result);
+#endif
