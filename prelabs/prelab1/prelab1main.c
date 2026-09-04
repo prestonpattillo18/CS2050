a5f83f3fe891f5c4ecb69856848cfed1b5a9e98a
prestonpattillo18 <preston.pattillo18@gmail.com>
2026-09-04 18:04:46 -0500

    Refactor array variable names for clarity

--- prelabs/prelab1/prelab1main.c
+++ prelabs/prelab1/prelab1main.c
@@ -5,47 +5,50 @@
 int main(){
     #define size 10
 
-    int arrayUnique3Small4Large8[size] = {5, 8, 4, 8, 8, 5, 4, 5, 4, 8};
-    int arrayUnique1Small7Large7[size] = {7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
-    int arrayUnique10small0Large9[size] = {5, 2, 8, 4, 6, 1, 9, 3, 7, 0};
+    //array names represent arrayUNIQUE_SMALL_LARGE
+    int array3_4_8[size] = {5, 8, 4, 8, 8, 5, 4, 5, 4, 8};
+    int array1_7_7[size] = {7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
+    int array10_0_9[size] = {5, 2, 8, 4, 6, 1, 9, 3, 7, 0};
 
     int special = 0;
     int tiny = 0;
     int big = 0;
-    int *specialPtr = &special;
-    int *tinyPtr = &tiny;
-    int *bigPtr = &big;
-    int *arrayPtr = arrayUnique10small0Large9;
+    int *sPtr = &special;
+    int *tPtr = &tiny;
+    int *bPtr = &big;
+    int *aPtr = array10_0_9;
+
+    //the next 4 sections are meant to return an error, this is really just to ensure that all parts of the validity checker work
+    sPtr = NULL;
+    if (getArrayInfo(aPtr, size, sPtr, tPtr, bPtr))
+        printf("Array info: Unique: %d (NA); Smallest: %d (NA); Largest %d (NA)\n", special, tiny, big);
     
-    specialPtr = NULL;
-    if (getArrayInfo(arrayPtr, size, specialPtr, tinyPtr, bigPtr))
-        printf("Array info: Unique: %d (NA); Smallest: %d (NA); Largest %d (NA)\n", special, tiny, big);
-    
-    specialPtr = &special;
-    tinyPtr = NULL;
-    if (getArrayInfo(arrayPtr, size, specialPtr, tinyPtr, bigPtr))
+    sPtr = &special;
+    tPtr = NULL;
+    if (getArrayInfo(aPtr, size, sPtr, tPtr, bPtr))
         printf("Array info: Unique: %d (NA); Smallest: %d (NA); Largest %d (NA)\n", special, tiny, big);
 
-    tinyPtr = &tiny;
-    bigPtr = NULL;
-    if (getArrayInfo(arrayPtr, size, specialPtr, tinyPtr, bigPtr))
+    tPtr = &tiny;
+    bPtr = NULL;
+    if (getArrayInfo(aPtr, size, sPtr, tPtr, bPtr))
         printf("Array info: Unique: %d (NA); Smallest: %d (NA); Largest %d (NA)\n", special, tiny, big);
 
-    bigPtr = &big;
-    arrayPtr = NULL;
-    if (getArrayInfo(arrayPtr, size, specialPtr, tinyPtr, bigPtr))
+    bPtr = &big;
+    aPtr = NULL;
+    if (getArrayInfo(aPtr, size, sPtr, tPtr, bPtr))
         printf("Array info: Unique: %d (NA); Smallest: %d (NA); Largest %d (NA)\n", special, tiny, big);
 
-    arrayPtr = arrayUnique3Small4Large8;
-    if (getArrayInfo(arrayPtr, size, specialPtr, tinyPtr, bigPtr))
+    //this section tests the 3 premade arrays, ensuring that they return the correct values. all data should match the parenthesis
+    aPtr = array3_4_8;
+    if (getArrayInfo(aPtr, size, sPtr, tPtr, bPtr))
         printf("Array info: Unique: %d (3); Smallest: %d (4); Largest %d (8)\n", special, tiny, big);
 
-    arrayPtr = arrayUnique1Small7Large7;
-    if (getArrayInfo(arrayPtr, size, specialPtr, tinyPtr, bigPtr))
+    aPtr = array1_7_7;
+    if (getArrayInfo(aPtr, size, sPtr, tPtr, bPtr))
         printf("Array info: Unique: %d (1); Smallest: %d (7); Largest %d (7)\n", special, tiny, big);
 
-    arrayPtr = arrayUnique10small0Large9;
-    if (getArrayInfo(arrayPtr, size, specialPtr, tinyPtr, bigPtr))
+    aPtr = array10_0_9;
+    if (getArrayInfo(aPtr, size, sPtr, tPtr, bPtr))
         printf("Array info: Unique: %d (10); Smallest: %d (0); Largest %d (9)\n", special, tiny, big);
 
     return 0;
