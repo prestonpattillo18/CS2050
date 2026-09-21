bb3ddde89cd63e0bd163c6f723c3fa3d156b8b8e
prestonpattillo18 <preston.pattillo18@gmail.com>
2026-09-21 14:30:28 -0500

    Add header file for smart array functions

---
+++ labs/lab3/lab3.h
@@ -0,0 +1,6 @@
+#ifndef _LAB_3_H
+#define _LAB_3_H
+void * allocateSmartArray(int elementSize, int elementCount);
+void freeSmartArray(void * smartArray);
+int getElementCount(void * smartArray);
+#endif
